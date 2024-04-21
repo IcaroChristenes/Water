4c25f1f6cbcb78521e302bb12c3554d033f0ab8c
IcaroChristenes <ichristenes71@gmail.com>
2024-04-21 17:24:35 -0300

    Main

---
+++ unijorge_trabalho.js
@@ -0,0 +1,8 @@
+function monkeyShow() {
+    let monkey = document.querySelector('.mobile_mn');
+    if (monkey.classList.contains('open')) {
+        monkey.classList.remove('open');
+    } else {
+        monkey.classList.add('open');
+    }
+}
